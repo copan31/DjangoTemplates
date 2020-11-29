b3148c9b8a9e30830c04e6e7f5dd462b945d170e
copan31 <52942558+copan31@users.noreply.github.com>
2020-11-29 16:32:15 +0900

    download 完了

--- templates/fileupdownld/forms.py
+++ templates/fileupdownld/forms.py
@@ -2,3 +2,16 @@ from django import forms
 
 class FileUpLdForm(forms.Form):
     upfile = forms.FileField()
+
+class ConfirmForm(forms.Form):
+    filename = forms.CharField(
+        max_length=100,
+    )
+    choice1 = forms.fields.ChoiceField(
+        choices = (
+            ('conv_a', '変換a'),
+            ('conv_b', '変換b'),
+        ),
+        required=True,
+        widget=forms.widgets.Select
+    )
--- templates/fileupdownld/views.py
+++ templates/fileupdownld/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import FileUpLdForm
+from .forms import FileUpLdForm, ConfirmForm
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import mimetypes
@@ -10,28 +10,34 @@ import os
 # Create your views here.
 def index(request):
 
-    if request.method == "POST":
+    if request.method == "POST" and request.FILES: # ファイルアップロード
         # POSTデータ取得
         upfile = request.FILES['upfile']
 
         # 保存
         fileobject = FileSystemStorage()
         filepath = os.path.join(settings.MEDIA_ROOT, upfile.name)
-        fileobject.save(filepath, upfile )
+        filename = fileobject.save(filepath, upfile )
 
-        # 変換
+        # 確認画面描画
+        context = {}
+        context['form'] = ConfirmForm(initial = {'filename' : filename})
+        return render( request, "confirm.html", context)
+   
+    elif request.method == "POST":
 
-        # 変換結果リターン
+        # 必要ならfileの内容に変換を加える
 
-        # ファイル名からmimetypeを推測。拡張子がないファイル等は、application/octet-stream
-        response = HttpResponse(content_type=mimetypes.guess_type(upfile.name)[0] or 'application/octet-stream')
-        # Content-Dispositionでダウンロードの強制
-        response['Content-Disposition'] = f'attachment; filename={upfile.name}'
-        # HttpResponseに、ファイルの内容を書き込む
-        shutil.copyfileobj(upfile, response)
-        
-        return response
-
+        # ファイルオープンして応答
+        file_path = request.POST.get("filename")
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as fh:
+                # ファイル名からmimetypeを推測。拡張子がないファイル等は、application/octet-stream
+                response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_path)[0] or 'application/octet-stream')
+                # Content-Dispositionでダウンロードの強制
+                response['Content-Disposition'] = f'attachment; filename={file_path}' 
+                return response
+        raise "Http404"
     else :
         context = {} 
         context['form'] = FileUpLdForm() 
